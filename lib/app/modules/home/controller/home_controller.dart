33dcc826a58034e22dc7aa0646983add5fae0cf1
pollyyana <pollyana.medeirosk@gmail.com>
2022-06-19 02:36:07 -0300

    implementar atualizar ao novo projeto

--- lib/app/modules/home/controller/home_controller.dart
+++ lib/app/modules/home/controller/home_controller.dart
@@ -9,7 +9,7 @@ import 'package:job_time/app/view_models/project_model.dart';
 part 'home_state.dart';
 
 class HomeController extends Cubit<HomeState> {
-  ProjectService _projectService;
+  final ProjectService _projectService;
 
   HomeController({required ProjectService projectService})
       : _projectService = projectService,
